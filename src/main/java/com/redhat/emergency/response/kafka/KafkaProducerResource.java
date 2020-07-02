df52322fb5b78d936dda4766a39ec50e6d2d050e
Bernard Tison <bernard.tison@gmail.com>
2020-07-02 13:44:01 +0200

    Support for Kafka message headers

--- src/main/java/com/redhat/emergency/response/kafka/KafkaProducerResource.java
+++ src/main/java/com/redhat/emergency/response/kafka/KafkaProducerResource.java
@@ -1,6 +1,10 @@
 package com.redhat.emergency.response.kafka;
 
+import java.nio.ByteBuffer;
+import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,6 +14,7 @@ import javax.ws.rs.core.Response;
 
 import io.smallrye.reactive.messaging.kafka.KafkaRecord;
 import io.vertx.core.json.JsonObject;
+import org.apache.kafka.common.header.Headers;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.slf4j.Logger;
@@ -38,15 +43,42 @@ public class KafkaProducerResource {
             log.error("Record value cannot be null or empty");
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
+        Map<String,Object> headers = json.getJsonObject("headers", new JsonObject()).getMap();
+        StringBuffer sb = new StringBuffer();
+        headers.forEach((key1, value1) -> sb.append(key1).append(":").append(value1.toString()).append(", "));
+        String hs = sb.toString();
+        if (hs.length() > 2) {
+            hs = hs.substring(0, hs.length()-2);
+        }
         log.info("Producing message");
         log.info("   Key: " + key);
+        log.info("   Headers: " + hs);
         log.info("   Value: " + value);
-        emitter.send(toMessage(key, json.getJsonObject("value").toString()));
+        emitter.send(toMessage(key, json.getJsonObject("value").toString(), headers));
         return Response.ok().build();
     }
 
-    private org.eclipse.microprofile.reactive.messaging.Message<String> toMessage(String key, String value) {
-        return KafkaRecord.of(key, value);
+    private org.eclipse.microprofile.reactive.messaging.Message<String> toMessage(String key, String value, Map<String, Object> headers) {
+        KafkaRecord<String, String> record = KafkaRecord.of(key, value);
+        Headers h = record.getHeaders();
+        headers.keySet().stream().collect(Collectors.toMap(Function.identity(), new Function<String, byte[]>() {
+            @Override
+            public byte[] apply(String s) {
+                Object value = headers.get(s);
+                if (value == null) {
+                    return new byte[]{};
+                }
+                if (value instanceof String) {
+                    return ((String) value).getBytes();
+                }
+                if (value instanceof Number) {
+                    long l = ((Number) value).longValue();
+                    return ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(l).array();
+                }
+                return value.toString().getBytes();
+            }
+        })).forEach(h::add);
+        return record;
     }
 
 }
--- src/test/java/com/redhat/emergency/response/kafka/KafkaProducerResourceTest.java
+++ src/test/java/com/redhat/emergency/response/kafka/KafkaProducerResourceTest.java
@@ -1,9 +1,11 @@
 package com.redhat.emergency.response.kafka;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 
@@ -13,7 +15,9 @@ import io.restassured.http.Header;
 import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
 import io.smallrye.reactive.messaging.connectors.InMemorySink;
 import io.smallrye.reactive.messaging.kafka.OutgoingKafkaRecord;
+import org.apache.kafka.common.header.Headers;
 import org.eclipse.microprofile.reactive.messaging.Message;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -34,7 +38,7 @@ public class KafkaProducerResourceTest {
 
         InMemorySink<String> results = connector.sink("channel");
 
-        String body = "{\"key\":\"key\",\"value\":{\"id\":\"id\"}}";
+        String body = "{\"key\":\"key\", \"headers\":{\"header1\":\"value1\",\"header2\":15}, \"value\":{\"id\":\"id\"}}";
 
         RestAssured.given().when().with().body(body).header(new Header("Content-Type", "application/json"))
                 .post("/produce")
@@ -47,6 +51,33 @@ public class KafkaProducerResourceTest {
         String key = ((OutgoingKafkaRecord<String, String>)message).getKey();
         assertThat(key, equalTo("key"));
         assertThat(value, equalTo("{\"id\":\"id\"}"));
+        Headers headers = ((OutgoingKafkaRecord<String, String>) message).getHeaders();
+        assertThat(headers.toArray().length, equalTo(2));
+        assertThat(headers.toArray()[0].key(), Matchers.anyOf(equalTo("header1"), equalTo("header2")));
+        assertThat(Arrays.stream(headers.toArray()).filter(h -> h.key().equals("header1")).findFirst().orElseThrow().value(), equalTo("value1".getBytes()));
+        assertThat(Arrays.stream(headers.toArray()).filter(h -> h.key().equals("header2")).findFirst().orElseThrow().value(), equalTo(ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(15L).array()));
+    }
+
+    @Test
+    void testProduceRecordNoHeaders() {
+
+        InMemorySink<String> results = connector.sink("channel");
+
+        String body = "{\"key\":\"key\", \"value\":{\"id\":\"id\"}}";
+
+        RestAssured.given().when().with().body(body).header(new Header("Content-Type", "application/json"))
+                .post("/produce")
+                .then().assertThat().statusCode(200).body(equalTo(""));
+
+        assertThat(results.received().size(), equalTo(1));
+        Message<String> message = results.received().get(0);
+        assertThat(message, instanceOf(OutgoingKafkaRecord.class));
+        String value = message.getPayload();
+        String key = ((OutgoingKafkaRecord<String, String>)message).getKey();
+        assertThat(key, equalTo("key"));
+        assertThat(value, equalTo("{\"id\":\"id\"}"));
+        Headers headers = ((OutgoingKafkaRecord<String, String>) message).getHeaders();
+        assertThat(headers.toArray().length, equalTo(0));
     }
 
 }
